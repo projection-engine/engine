432963d3917b1fe2a069639b18cc043e4f97ecdf
facobackup <gustavomicaelbarbosa@gmail.com>
2022-06-23 18:36:40 -0300

    [MIX] - [Material uniforms fix; Accordion visual tweaks; Roboto font fixed; Gizmos size fixed; Fixed gizmo location ortho projection; fixed gizmo picking ortho projection;]

--- shaders/PICK.glsl.js
+++ shaders/PICK.glsl.js
@@ -8,6 +8,7 @@ uniform vec3 translation;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
 uniform mat4 transformMatrix;
+uniform bool cameraIsOrthographic;
 
 void main() {
  vec3 t = translation - camPos;
@@ -20,15 +21,17 @@ void main() {
     for ( int x = 0; x < 4; x++ )
         for ( int y = 0; y < 4; y++ )
             if ( x == y && x <= 2 )
-                sc[x][y] = len;
+                sc[x][y] = cameraIsOrthographic ? 1.75 : len;
             else if ( x == y )
                 sc[x][y] = 1.;
             else
                 sc[x][y] = 0.;  
                 
-    tt[3][0]  += t.x;
-    tt[3][1]  += t.y;
-    tt[3][2]  += t.z;
+	if(!cameraIsOrthographic){
+		tt[3][0]  += t.x;
+		tt[3][1]  += t.y;
+		tt[3][2]  += t.z;
+	}
     
     gl_Position =  projectionMatrix * viewMatrix * tt * sc * vec4(position,1.0);   
 }
