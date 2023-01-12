aa2123b7d21ec8b5df6e60694ed0d2b5bfea99e2
facobackup <gustavomicaelbarbosa@gmail.com>
2023-01-12 17:11:27 -0300

    - Adding decal component to structure

---
+++ instances/components/DecalComponent.ts
@@ -0,0 +1,7 @@
+import Component from "./Component";
+import DECAL_PROPS from "../../static/component-props/DECAL_PROPS";
+
+export default class DecalComponent extends Component {
+    _props = DECAL_PROPS
+    imageID?: string
+}
---
+++ static/component-props/DECAL_PROPS.ts
@@ -0,0 +1,7 @@
+import Component from "../../instances/components/Component";
+
+export default [
+    Component.group("TEXTURE", [
+        Component.imageTexture("IMAGE", "imageID"),
+    ])
+]
