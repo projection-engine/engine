e7996fee45bedce91f87d3d4e28ad2c4162afaa5
facobackup <gustavomicaelbarbosa@gmail.com>
2023-01-14 16:28:46 -0300

    - Fixing bugs on shader editor related to new attributes - Fixed shader editor not updating material settings -

--- static/MATERIAL_RENDERING_TYPES.ts
+++ static/MATERIAL_RENDERING_TYPES.ts
@@ -7,3 +7,13 @@ export default {
     TRANSPARENCY: 5,
     SKY: 6
 }
+
+export const INVERSE_MATERIAL_RENDERING_TYPES = {
+    0: "Unlit",
+    1: "Isotropic",
+    2: "Anisotropic",
+    3: "Sheen",
+    4: "Clear coat",
+    5: "Transparency",
+    6: "Sky"
+}
